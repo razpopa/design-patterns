e056c092413c97441bf26a6922f8f9cc23544d09
Razvan P <razvan.popa82@gmail.com>
2021-12-15 13:57:44 +0200

    Initialize solution

---
+++ DesignPatterns/DesignPatterns/ServiceLayer/MenuOrchestrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsApp.ServiceLayer
+{
+    public class MenuOrchestrator
+    {
+
+        private int PrintHomeMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t WELCOME TO THE DESIGN PATTERNS APP\n" +
+                "1. Creational\n" +
+                "2. Behavioral\n" +
+                "3. Structural\n");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
